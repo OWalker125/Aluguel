7f58d6825e3cbc5195265e362091ac9954ac472b
Thiago Sousa <thiagosilvadesousa@edu.unirio.br>
2023-01-05 12:38:00 -0300

    update tests

--- src/test/java/com/aluguel/nossa_bike/controllers/NossaBikeControllerTest.java
+++ src/test/java/com/aluguel/nossa_bike/controllers/NossaBikeControllerTest.java
@@ -1,41 +1,82 @@
-/*package com.aluguel.nossa_bike.controllers;
+package com.aluguel.nossa_bike.controllers;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
 
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpMethod;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
+import com.aluguel.nossa_bike.models.Ciclista;
+import com.aluguel.nossa_bike.models.Passaport;
+import com.aluguel.nossa_bike.models.Ciclista.Nacionalidade;
+import com.aluguel.nossa_bike.models.Ciclista.Status;
+import com.aluguel.nossa_bike.repository.CiclistaRepository;
+import com.aluguel.nossa_bike.services.CiclistaService;
 
-@SpringBootApplication
 public class NossaBikeControllerTest {
 
-    @Autowired
+    @InjectMocks
     NossaBikeController controller;
     
-  @Test
-    public void olaTestt() {
-    LinkedList<String> listaZeroErro = new LinkedList<String>();
-    cadastroMock = mock(Cadastro.class);
-    Ciclista ciclista = new Ciclista();
+    @Mock
+    CiclistaRepository dbCiclista;
+    @Mock
+    CiclistaService cicService;
 
-    when(cadastroMock.isValid(any())).thenReturn(listaZeroErro);
-    assertEquals(listaZeroErro, controller.cadastrar(ciclista));
-  }*/
 
-  /*@Test
-    public void testIT(){
-            
-    RestTemplate restTemplate = new RestTemplateBuilder().rootUri("http://localhost:8081/").build();
-    Client client = new Client(1,"teste");        
-    ResponseEntity<String> response = restTemplate.exchange("/client", HttpMethod.POST,, null);
-            
-    assertEquals("Olá, mundo.", response.getBody());
-  }
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
 
-}*/
+    @Test
+    public void whenNoErrorsThenZeroErrorsAndCreated(){
+        List<String> listaVazia = new LinkedList<String>();
+        when(cicService.cadastrar(any())).thenReturn(listaVazia);
+        when(cicService.editAccount(any(),any())).thenReturn(listaVazia);
+        ResponseEntity esperado = new ResponseEntity<>(listaVazia, HttpStatus.CREATED);
+        assertEquals(esperado, controller.cadastrar(any()));
+        assertEquals(esperado, controller.accountEdit(any(), any()));
+    }
+
+    @Test
+    public void whenErrorThenErrorsAndBadRequest(){
+        List<String> erro = new LinkedList<String>();
+        erro.add("Nome Inválido");
+        when(cicService.cadastrar(any())).thenReturn(erro);
+        when(cicService.editAccount(any(),any())).thenReturn(erro);
+        ResponseEntity esperado = new ResponseEntity<>(erro, HttpStatus.BAD_REQUEST);
+        assertEquals(esperado, controller.cadastrar(any()));
+        assertEquals(esperado, controller.accountEdit(any(), any()));
+    }
+
+    @Test
+    public void whenValidThenZeroErrorsAndOk(){
+        Passaport passaport = new Passaport(1, 1, "01/12/2001", "Brasil");
+        Ciclista ciclista = new Ciclista(UUID.randomUUID(), "Thiago", Status.INATIVO, "23/02/2000", "473.296.280-77", passaport,
+        Nacionalidade.BRASILEIRO, "teste@teste.com", "https://teste.net");
+        when(cicService.activateAccount(null)).thenReturn(ciclista);
+
+        ResponseEntity esperado = new ResponseEntity<>(ciclista, HttpStatus.OK);
+        assertEquals(esperado, controller.accountActivation(any())); 
+    }
+
+    @Test
+    public void whenNullThenErrorAndNotFound(){
+        when(cicService.activateAccount(null)).thenReturn(null);
+
+        ResponseEntity esperado = new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        assertEquals(esperado, controller.accountActivation(any())); 
+    }
+
+}
